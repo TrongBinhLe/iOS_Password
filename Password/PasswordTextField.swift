73c33285519443a164025354dcffb2f6b2f4b6fc
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-04-19 14:25:39 +0700

    feature: Adding eyebutton for password text field

--- Password/PasswordTextField.swift
+++ Password/PasswordTextField.swift
@@ -11,9 +11,14 @@ import UIKit
 class PasswordTextField: UIView {
     
     let lockImageView = UIImageView(image: UIImage(systemName: "lock.fill"))
+    let textField = UITextField()
+    let placeHolderText: String
+    let eyeButton = UIButton(type: .custom)
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(placeHolderText: String) {
+        self.placeHolderText = placeHolderText
+        super.init(frame: .zero)
+        
         style()
         layout()
     }
@@ -34,14 +39,49 @@ extension PasswordTextField {
     private func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemOrange
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.isSecureTextEntry = false
+        textField.placeholder = placeHolderText
+//        textField.delegate = self
+        textField.keyboardType = .asciiCapable
+        textField.attributedPlaceholder = NSAttributedString(string: placeHolderText, attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryLabel])
+        
+        eyeButton.translatesAutoresizingMaskIntoConstraints = false
+        eyeButton.setImage(UIImage(systemName: "eye.circle"), for: .normal)
+        eyeButton.setImage(UIImage(systemName: "eye.slash.circle"), for: .selected)
+        eyeButton.addTarget(self, action: #selector(togglePasswordView), for: .touchUpInside)
     }
     
     private func layout() {
         addSubview(lockImageView)
+        addSubview(textField)
+        addSubview(eyeButton)
         
+        //lock
         NSLayoutConstraint.activate([
-            lockImageView.topAnchor.constraint(equalTo: topAnchor),
+            lockImageView.centerYAnchor.constraint(equalTo: textField.centerYAnchor),
             lockImageView.leadingAnchor.constraint(equalTo: leadingAnchor)
         ])
+        
+        // textField
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: topAnchor),
+            textField.leadingAnchor.constraint(equalToSystemSpacingAfter: lockImageView.trailingAnchor, multiplier: 1)
+        ])
+        
+        // eye
+        NSLayoutConstraint.activate([
+            eyeButton.centerYAnchor.constraint(equalTo: textField.centerYAnchor),
+            eyeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: textField.trailingAnchor, multiplier: 1),
+            eyeButton.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
+extension PasswordTextField {
+    @objc func togglePasswordView(_ sender: Any) {
+        textField.isSecureTextEntry.toggle()
+        eyeButton.isSelected.toggle()
     }
 }
