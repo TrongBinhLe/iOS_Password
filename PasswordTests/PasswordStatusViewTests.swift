e17e5e34956d54bccdc7d4639d675ef05b9ddbab
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-05-12 16:25:53 +0700

    Update unit test Password Status View

--- PasswordTests/PasswordStatusViewTests.swift
+++ PasswordTests/PasswordStatusViewTests.swift
@@ -14,7 +14,7 @@ class PasswordStatusViewTests_ShowCheckmarkOrReset_When_Validation_Is_Inline: XC
     
     var statusView: PasswordStatusView!
     let validPassword = "12345678Aa!"
-    let toShortPassword = "123"
+    let to0ShortPassword = "123"
     
     override func setUp() {
         super.setUp()
@@ -22,9 +22,33 @@ class PasswordStatusViewTests_ShowCheckmarkOrReset_When_Validation_Is_Inline: XC
         statusView.shouldResetCriteria = true
     }
     
-    func testsValidPassword() {
+    func testValidPassword() {
         statusView.updateDisplay(validPassword)
         XCTAssertTrue(statusView.lengthCriteriaView.isCriteriaMet)
         XCTAssertTrue(statusView.lengthCriteriaView.isCheckMarkImage)
     }
+}
+
+class PasswordStatusViewTests_showCheckmarkOrRedX_When_Validation_Is_Loss_Of_Focus: XCTestCase {
+    var statusView: PasswordStatusView!
+    let validPassword = "12345678Aa!"
+    let tooShortPassword = "123"
+    
+    override func setUp() {
+        super.setUp()
+        statusView = PasswordStatusView()
+        statusView.shouldResetCriteria = false
+    }
+    
+    func testValidPassword() throws {
+        statusView.updateDisplay(validPassword)
+        XCTAssertTrue(statusView.lengthCriteriaView.isCriteriaMet)
+        XCTAssertTrue(statusView.lengthCriteriaView.isCheckMarkImage)
+    }
+    
+    func testTooShort() throws {
+        statusView.updateDisplay(tooShortPassword)
+        XCTAssertFalse(statusView.lengthCriteriaView.isCriteriaMet)
+        XCTAssertTrue(statusView.lengthCriteriaView.isXmarkImage)
+    }   
 }
